31cc3e0ed81cbde55726d0157d3d0ced2500dfd7
AnaZ <znaor.a@gmail.com>
2022-05-23 03:52:22 +0200

    testing

---
+++ Models/Repositories/KorisnikRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kafic.Models.Repositories
+{
+    public class KorisnikRepository
+    {
+        private KaficContext ctx;
+
+        public KorisnikRepository(KaficContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public int InsertKorisnik(Korisnik korisnik)
+        {
+            ctx.Korisnik.Add(korisnik);
+            ctx.SaveChanges();
+
+            return korisnik.IdKorisnik;
+        }
+
+        public IEnumerable<Korisnik> GetKorisnik()
+        {
+            return ctx.Korisnik.ToList();
+        }
+
+        public Korisnik GetKorisnikById(int id)
+        {
+            return ctx.Korisnik.Find(id);
+        }
+
+        public void DeleteKorisnik(int id)
+        {
+            Korisnik korisnik = ctx.Korisnik.Find(id);
+            ctx.Korisnik.Remove(korisnik);
+            ctx.SaveChanges();
+        }
+
+      
+
+
+    }
+}
